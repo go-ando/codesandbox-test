f886dbe3bea77374e718b2a70cacb6dac9611640
go-ando <52556625+go-ando@users.noreply.github.com>
2021-03-11 12:29:48 +0900

    todo complete

--- src/index.js
+++ src/index.js
@@ -5,14 +5,62 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
+  createIncompleteList(inputText);
+};
+
+const deleteFromIncompleteList = (target) => {
+  document.getElementById("incomplate-list").removeChild(target);
+};
+
+const createIncompleteList = (text) => {
   const div = document.createElement("div");
   div.className = "list-row";
 
   const li = document.createElement("li");
-  li.innerText = inputText;
+  li.innerText = text;
+
+  const complateButton = document.createElement("button");
+  complateButton.innerText = "完了";
+  complateButton.addEventListener("click", () => {
+    //親タグ削除
+    deleteFromIncompleteList(complateButton.parentNode);
+    //完了リストに追加
+    const addTarget = complateButton.parentNode;
+    //内容取得
+    const text = addTarget.firstElementChild.innerText;
+    addTarget.textContent = null;
+    //li生成
+    const li = document.createElement("li");
+    li.innerText = text;
+
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      //親タグ完了リストから削除
+      const deleteButton = backButton.parentNode;
+      document.getElementById("complete-list").removeChild(deleteButton);
+      //テキスト取得
+      const text = backButton.parentNode.firstElementChild.innerText;
+      createIncompleteList(text);
+    });
+
+    addTarget.appendChild(li);
+    addTarget.appendChild(backButton);
+
+    document.getElementById("complete-list").appendChild(addTarget);
+  });
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    //親タグ削除
+    deleteFromIncompleteList(deleteButton.parentNode);
+  });
+  //console.log(deleteButton);
 
   div.appendChild(li);
-  //console.log(li);
+  div.appendChild(complateButton);
+  div.appendChild(deleteButton);
   document.getElementById("incomplate-list").appendChild(div);
 };
 
